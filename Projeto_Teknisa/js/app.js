62eb78628d4e615dca08587320fcb11239d54c1b
Lorayne <lorayneod@gmail.com>
2022-03-18 01:03:51 -0300

    Finish

--- Projeto_Teknisa/js/app.js
+++ Projeto_Teknisa/js/app.js
@@ -1,10 +1,10 @@
-function preRender(){
+function preRender() {
     let countVisibleCards = getCountVisibleCards();
     updateResults(countVisibleCards);
 }
 
-function getCountVisibleCards(){
-    return Array.from(document.getElementsByClassName("card")).filter((card) => !card.getElementsByClassName.display || card.getElementsByClassName.display !=="none").length;
+function getCountVisibleCards() {
+    return Array.from(document.getElementsByClassName("card")).filter((card) => !card.getElementsByClassName.display || card.getElementsByClassName.display !== "none").length;
 }
 
 function updateResults(count) {
@@ -12,13 +12,13 @@ function updateResults(count) {
 }
 
 function filter() {
-    let {search, operation, languages} = getFilterProperties();
+    let { search, operation, languages } = getFilterProperties();
     let interval = setInterval((_) => {
-        let[containerEl] = document.getElementsByClassName("container");
+        let [containerEl] = document.getElementsByClassName("container");
         let changedText = search !== getSearchValue();
-        if(!changedText) clearInterval(interval);
-        if(containerEl && containerEl.children && !changedText) {
-            let visibleCards = updateVisibleCards(containerEl,search,operation, languages);
+        if (!changedText) clearInterval(interval);
+        if (containerEl && containerEl.children && !changedText) {
+            let visibleCards = updateVisibleCards(containerEl, search, operation, languages);
             updateResults(visibleCards);
         }
     }, 800);
@@ -26,7 +26,7 @@ function filter() {
 
 function getFilterProperties() {
     let search = getSearchValue();
-    let[radio] = getSelectedRadio();
+    let [radio] = getSelectedRadio();
     let operation = radio.id == "1" ? "AND" : "OR";
     let languages = Array.from(getSelectedLanguages()).map((lang) => lang.name);
     return {
@@ -36,42 +36,42 @@ function getFilterProperties() {
     }
 }
 
-function getSearchValue(){
+function getSearchValue() {
     let inputSearchEl = document.getElementById("nameSearch");
     return inputSearchEl.value;
 }
 
-function getSelectedRadio(){
+function getSelectedRadio() {
     return Array.from(document.querySelectorAll('header input[type="radio"]:checked'));
 }
 
-function getSelectedLanguages(){
+function getSelectedLanguages() {
     return Array.from(document.querySelectorAll('header input[type="checkbox"]:checked'));
 }
 
-function updateVisibleCards(containerEl, search, operation, selectedLanguages){
+function updateVisibleCards(containerEl, search, operation, selectedLanguages) {
     let visibleCards = 0;
     Array.from(containerEl.children).forEach((cardEl) => {
         let [titleEl] = cardEl.getElementsByClassName("card-title");
         let cardLanguages = Array.from(cardEl.getElementsByClassName("iconLanguage")).map((image) => image.name);
-        if(titleEl) {
+        if (titleEl) {
             let isMatchName = isMatchByName(titleEl.textContent, search);
-            if(!isMatchName && operation == "AND"){
+            if (!isMatchName && operation == "AND") {
                 hideCard(cardEl);
-            } else if(isMatchName && operation == "OR") {
+            } else if (isMatchName && operation == "OR") {
                 showCard(cardEl);
                 visibleCards++;
-            } else if(isMatchName && operation == "AND"){
+            } else if (isMatchName && operation == "AND") {
                 let isMatchLanguage = isMatchByLanguage(cardLanguages, selectedLanguages);
-                if(isMatchLanguage) {
+                if (isMatchLanguage) {
                     showCard(cardEl);
                     visibleCards++;
-                } else{
+                } else {
                     hideCard(cardEl);
                 }
             } else if (!isMatchName && operation == "OR") {
                 let isMatchLanguage = isMatchByLanguage(cardLanguages, selectedLanguages);
-                if(isMatchLanguage){
+                if (isMatchLanguage) {
                     showCard(cardEl);
                     visibleCards++;
                 } else {
@@ -87,7 +87,7 @@ function isMatchByName(textCard, textInput) {
     return textCard.toLowerCase().includes(textInput.toLowerCase());
 }
 
-function isMatchByLanguage(cardLanguages, selectedLanguages){
+function isMatchByLanguage(cardLanguages, selectedLanguages) {
     return cardLanguages.some(cardLang => selectedLanguages.includes(cardLang));
 }
 
@@ -103,22 +103,22 @@ const modalTemplate = `
       <div class="modal">
         <button class="fechar">x</button>
         <div class="profilePictureModal">
-            <img class="image" name ="image-profile" src="__DEV_IMAGE__" alt="desenvolvedor" />
+            <img class="image" name ="image-profile" src='_DEV_IMAGE_' alt="desenvolvedor" />
         </div>
         <div class="profileDescriptionModal">
             <div style="display: flex; flex-direction: column">
-                <h2 class="card-title" name="devname">__DEV_NAME__</h2>
-                <span name="age">__DEV_AGE__</span>
-                <span name="description">__DEV_DESCRIPTION__</span>
-                <span name="description">__DEV_DESCRIPTION_ABILITY__</span>
+                <h2 class="card-title" name="devname">_DEV_NAME_</h2>
+                <span name="age">_DEV_AGE_</span>
+                <span name="description">_DEV_DESCRIPTION_</span>
+                <span name="descriptionAbility">_DEV_DESCRIPTION_ABILITY_</span>
                 <h3>Contato</h3>
-                <span name="mail">Email: <a href="#">__DEV_MAIL__</a></span>
-                <span name="git">Github: <a href="#">__DEV_GIT__</a></span>
-                <span name="phone">Telefone:__DEV_PHONE__</span>
+                <span name="mail">Email: <a href="#">_DEV_MAIL_</a></span>
+                <span name="git">Github: <a href="#">_DEV_GIT_</a></span>
+                <span name="phone">Telefone:_DEV_PHONE_</span>
             </div>
             <div class="languages">
             <h3>Linguagens</h3>
-                __DEV_LANGUAGES__
+                _DEV_LANGUAGES_
             </div>
         </div>
       </div>
@@ -131,10 +131,10 @@ Array.from(document.querySelectorAll('.card')).forEach(card => {
 function iniciaModal(modalId, cardId) {
     fillModal(getUserInfo(cardId));
     const modal = document.getElementById(modalId);
-    if(modal) {
+    if (modal) {
         modal.classList.add("mostrar");
         modal.addEventListener("click", (e) => {
-            if(e.target.id == modalId || e.target.className == "fechar") {
+            if (e.target.id == modalId || e.target.className == "fechar") {
                 modal.classList.remove("mostrar")
             }
         })
@@ -144,20 +144,19 @@ function iniciaModal(modalId, cardId) {
 function getUserInfo(id) {
     let cardUser = document.getElementById(id);
     let userData = {};
-    if(cardUser) {
-        userData = ['age', 'mail', 'phone', 'github', 'username', 'description', 'descriptionAbility'].reduce((acc,name) => {
-            userData.languages = 
-                Array.from(cardUser.querySelectorAll("languages > .iconLanguage")).reduce(function(acc, el) {
-                    let nameLanguage = el.name;
-                    acc[nameLanguage] = `${el.getAttribute('experience')}`;
-                    return acc;
-                }, {});
-                userData.picture = cardUser.querySelector( ".profilePicture > img").getAttribute('src');
-            acc[name] = getTextContentByName(cardUser,name);
+    if (cardUser) {
+        userData = ['age', 'mail', 'phone', 'github', 'username', 'description', 'descriptionAbility'].reduce((acc, name) => {
+            acc[name] = getTextContentByName(cardUser, name);
             return acc;
-    } );
-    return userData;
-    } 
+        }, {});
+        userData.languages = Array.from(cardUser.querySelectorAll("languages, .iconLanguage")).reduce(function (acc, el) {
+                let nameLanguage = el.name;
+                acc[nameLanguage] = `${el.getAttribute('experience')}`;
+                return acc;
+            }, {});
+        userData.picture = cardUser.querySelector(".profilePicture > img").getAttribute('src');
+        return userData;
+    }
 }
 
 function getTextContentByName(el, name, defaultValue = "") {
@@ -165,10 +164,9 @@ function getTextContentByName(el, name, defaultValue = "") {
     return element ? element.textContent : defaultValue;
 }
 
-function fillModal(userInfo){
+function fillModal(userInfo) {
     let descriptionLanguages = getDescriptionLanguage();
-    
-    languages = Object.keys(userInfo.descriptionLanguages).map((langCode => {
+    languages = Object.keys(userInfo.languages).map(langCode => {
         return `
             <div class="languageDescription">
                 <div class="language-name">
@@ -176,16 +174,17 @@ function fillModal(userInfo){
                     <span>${descriptionLanguages[langCode]}</span>
                 </div>
                 <div class="modal-experience">
-                    <span>${userInfo.languages[langCode]} + Anos</span>
+                    <span>${userInfo.languages[langCode]} Anos</span>
                 </div>
             </div>
         `;
     })
-    .join("\n"));
+        .join("\n");
     modal = modalTemplate;
     modal = modal.replaceAll('_DEV_IMAGE_', userInfo.picture);
     modal = modal.replaceAll('_DEV_NAME_', userInfo.username);
     modal = modal.replaceAll('_DEV_AGE_', userInfo.age);
+    
     modal = modal.replaceAll('_DEV_DESCRIPTION_', userInfo.description);
     modal = modal.replaceAll('_DEV_DESCRIPTION_ABILITY_', userInfo.descriptionAbility);
     modal = modal.replaceAll('_DEV_MAIL_', userInfo.mail);
@@ -195,7 +194,7 @@ function fillModal(userInfo){
     document.querySelector('#modal-profile').innerHTML = modal;
 };
 
-function getDescriptionLanguage( ) {
+function getDescriptionLanguage() {
     let description = {
         js: "JavaScript",
         php: "PHP",
